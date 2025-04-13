344fc7b6b7e24010cc2039c8c3c790bc5df42276
hasnainmalik3110 <hasnainmalik3110@gmail.com>
2025-04-13 10:30:04 +0500

    path change

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },  // Email field added
+  email: { type: String, required: true, unique: true }, 
   passkey: Object,
 });
 
